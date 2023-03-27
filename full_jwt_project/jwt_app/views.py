767b9322b85677a77defa41df4186674a0a3840d
sumeetptl <thesumeetpail@gmail.com>
2023-03-27 12:51:53 +0530

    Add basic structure for register API

--- full_jwt_project/jwt_app/views.py
+++ full_jwt_project/jwt_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+class UserRegisterationView(APIView):
+    def post(self,request,format=None):
+        return Response(data={'msg':"User Registered Succesfully"},status=status.HTTP_201_CREATED)
