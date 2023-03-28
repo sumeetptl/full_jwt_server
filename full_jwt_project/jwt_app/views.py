aff00fb4a712d1b3454fca2a74010a43b3b78529
sumeetptl <thesumeetpail@gmail.com>
2023-03-28 16:36:15 +0530

    Login API

--- full_jwt_project/jwt_app/views.py
+++ full_jwt_project/jwt_app/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from jwt_app.serializers import UserRegistrationSerializer
+from jwt_app.serializers import UserRegistrationSerializer, UserLoginSerializer
+from jwt_app.models import User
 
 class UserRegisterationView(APIView):
     def post(self,request,format=None):
@@ -9,4 +10,20 @@ class UserRegisterationView(APIView):
         if serializer.is_valid(raise_exception=True):
             user = serializer.create(request.data)
             return Response(data=serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(APIView):
+    def post(self,request,format=None):
+        data=request.data
+        serializer=UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            email = data.get("email")
+            password = data.get("password")
+            try:
+                user=User.objects.get(email=email)
+            except User.DoesNotExist:
+                return Response({"msg":"User Doesn't Exist"},status=status.HTTP_400_BAD_REQUEST)
+            if user.password == password:
+                return Response({"msg":"Login Succesful"},status=status.HTTP_200_OK)
+            return Response({"msg":"Invalid Password"},status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
