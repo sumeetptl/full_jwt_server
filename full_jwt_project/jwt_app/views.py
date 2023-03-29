b466abaed411876aea05498a7cf6f5794b6522e8
sumeetptl <thesumeetpail@gmail.com>
2023-03-29 18:40:01 +0530

    Change Password For Logged in User

--- full_jwt_project/jwt_app/views.py
+++ full_jwt_project/jwt_app/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from jwt_app.serializers import UserProfileSerializer, UserRegistrationSerializer, UserLoginSerializer
+from jwt_app.serializers import UserProfileSerializer, UserRegistrationSerializer, UserLoginSerializer, ChangePasswordSerializer
 from jwt_app.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
@@ -48,4 +48,12 @@ class UserProfileView(APIView):
         print("Request",request.user)
         serializer= UserProfileSerializer(request.user)
         return Response(serializer.data,status=status.HTTP_200_OK)      
-    
+
+class UserChangePassword(APIView):
+    renderer_classes=[UserRenderer]
+    permission_classes=[IsAuthenticated]
+    def post(self,request,format=None):
+        serializer = ChangePasswordSerializer(data=request.data,context={'user':request.user})
+        if serializer.is_valid(raise_exception=True):
+            return Response({"msg":"Password Changed Succesfully","status":status.HTTP_200_OK})    
+        return Response(serializer.errors,status.HTTP_400_BAD_REQUEST) 
