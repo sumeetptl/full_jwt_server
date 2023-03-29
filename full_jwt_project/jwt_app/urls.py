af1ebe369bab69bbd922ee82ab9cceed763310a8
sumeetptl <thesumeetpail@gmail.com>
2023-03-29 18:40:44 +0530

    Change Password Serialized

--- full_jwt_project/jwt_app/urls.py
+++ full_jwt_project/jwt_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from jwt_app.views import UserRegisterationView, UserLoginView, UserProfileView
+from jwt_app.views import UserRegisterationView, UserLoginView, UserProfileView, UserChangePassword
 
 urlpatterns = [
     path('register/',UserRegisterationView.as_view(), name="Registeration" ),
     path('login/',UserLoginView.as_view(), name="Login" ),
-    path('profile/',UserProfileView.as_view(), name="Profile")
+    path('profile/',UserProfileView.as_view(), name="Profile"),
+    path('change_password/',UserChangePassword.as_view(), name="Change_password"),
+    
 
 ]
